6cc50020a2e4c8c5a84484338aaee4a9c442dbbb
cmdFrog <cmdfroggit@gmail.com>
2024-06-08 22:39:42 -0700

    Finished text_node_to_html_node function to with currently used text_types Some unittests for the function added to test_textnode.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,30 @@
 from textnode import TextNode
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import LeafNode
 
 
 def main():
     print("Quiche Eater")
 
-def text_node_to_html_node(text_node):
-    pass
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    tag_val_nodes = {"italic": "i", "bold": "b", "code": "code"}
+
+    if text_node.text_type in tag_val_nodes:
+        return LeafNode(tag=tag_val_nodes[text_node.text_type], value=text_node.text,)
+
+    if text_node.text_type == "text":
+        return LeafNode(value=text_node.text)
+
+    if text_node.text_type == "link":
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+
+    if text_node.text_type == "image":
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+
+    raise ValueError("Incompatible or no text_type in text_node_to_html_node input")
 
 
 
 
 
-main()
+if __name__ == "__main__":
+    main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,8 @@
 import unittest
 
 from textnode import TextNode
+from main import text_node_to_html_node
+from htmlnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -20,6 +22,17 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold", "boot.dev")
         self.assertIsNone(node.url)
         self.assertIsNotNone(node2.url)
+
+    def test_text_to_html_node(self):
+        node1 = TextNode(text_type="bold", text="TEXT GOES HERE :D")
+        node2 = TextNode(text_type="link", text="ANCHOR TEXT", url="www.google.com")
+        node3 = TextNode(text_type="image", text="IMAGE DESCRIPTION", url="SOURCE/OF/IMAGE.JPG")
+        ans1 = LeafNode(tag="b", value="TEXT GOES HERE :D", props=None)
+        ans2 = LeafNode(tag="a", value="ANCHOR TEXT", props={'href': 'www.google.com'})
+        ans3 = LeafNode(tag="img", value="", props={'src': 'SOURCE/OF/IMAGE.JPG', 'alt': 'IMAGE DESCRIPTION'})
+        self.assertEqual(str(text_node_to_html_node(node1)), str(ans1))
+        self.assertEqual(str(text_node_to_html_node(node2)), str(ans2))
+        self.assertEqual(str(text_node_to_html_node(node3)), str(ans3))
 
 if __name__ == "__main__":
     unittest.main()

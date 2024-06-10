dbad2b2968d6193e98f177989ea91840336ca500
cmdFrog <cmdfroggit@gmail.com>
2024-06-10 14:22:54 -0700

    added block_to_block_type function and unittests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,10 +1,51 @@
 
-
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
 
 def markdown_to_blocks(markdown: str) -> list:
     final_list = []
     split = markdown.split('\n\n')
     for string in split:
         if string:
-            final_list.append(string)
+            final_list.append(string.strip(" "))
     return final_list
+
+def block_to_block_type(block: str) -> str:
+    split = block.split("\n")
+    if (block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+        ):
+        return block_type_heading
+    if len(split) > 1 and split[0].startswith("```") and split[-1].startswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        for line in split:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* "):
+        for line in split:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_unordered_list
+    if block.startswith("- "):
+        for line in split:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_unordered_list
+    if block.startswith("1. "):
+        i = 1
+        for line in split:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_ordered_list
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, block_type_code, block_type_heading, block_type_ordered_list, block_type_paragraph
 
 class TestMarkdownBlocks(unittest.TestCase):
 
@@ -12,5 +12,20 @@ This is the same paragraph on a new line
 
 * This is a list
 * with items """
-        ans = [' This is **bolded** paragraph', 'This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line', '* This is a list\n* with items ']
+        ans = ['This is **bolded** paragraph', 'This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line', '* This is a list\n* with items']
         self.assertEqual(str(markdown_to_blocks(test_string)), str(ans))
+
+    def test_block_to_block_type(self):
+        test_line = """```
+This is code
+```"""
+        test_line_2 = """##### HEADING"""
+        test_line_3 = """1. Item 1
+2. Item 2
+3. Item 3"""
+        test_line_4 = """This is a paragraph"""
+        test_list = [test_line, test_line_2, test_line_3, test_line_4]
+        ans_list = [block_type_code, block_type_heading, block_type_ordered_list, block_type_paragraph]
+        for test, ans in zip(test_list, ans_list):
+            #print(test, ans)
+            self.assertEqual(block_to_block_type(test), ans)

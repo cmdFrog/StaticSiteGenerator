fbf80ae1c9932bcd43395eb775810dca610b7aa9
cmdFrog <cmdfroggit@gmail.com>
2024-06-16 05:35:03 -0700

    recursive function for page generator. All MD pages in content directory are now made into HTML pages at once and work properly on the server.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from page_gen import copy_contents_to, generate_page
+from page_gen import copy_contents_to, generate_pages_recursive
 
 def main():
     print("Main Started")
     copy_contents_to("/home/cmdfrog/workspace/github.com/cmdFrog/SSG/static", "/home/cmdfrog/workspace/github.com/cmdFrog/SSG/public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content/", "template.html", "public/")
 
 
 if __name__ == "__main__":
--- src/page_gen.py
+++ src/page_gen.py
@@ -62,4 +62,20 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w", encoding="utf-8") as file:
         file.write(final_html)
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    abs_path_content = relative_to_absolute(dir_path_content)
+    abs_path_dest = relative_to_absolute(dest_dir_path)
+    file_dir = os.listdir(abs_path_content)
+    for item in file_dir:
+        abs_path_item = os.path.join(abs_path_content, item)
+        if os.path.isdir(abs_path_item):
+            dest_subdir = os.path.join(abs_path_dest, item)
+            os.mkdir(dest_subdir)
+            generate_pages_recursive(abs_path_item, template_path, dest_subdir)
+        elif os.path.isfile(abs_path_item) and item.endswith('.md'):
+            dest_file_path = os.path.join(abs_path_dest, os.path.splitext(item)[0] + '.html')
+            generate_page(abs_path_item, template_path, dest_file_path)
+
+
+
 #generate_page("/home/cmdfrog/workspace/github.com/cmdFrog/SSG/content/index.md", "/home/cmdfrog/workspace/github.com/cmdFrog/SSG/template.html", "/home/cmdfrog/workspace/github.com/cmdFrog/SSG/public/index.html")
